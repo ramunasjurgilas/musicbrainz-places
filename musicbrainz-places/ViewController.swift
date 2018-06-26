ce8a1cee0575c909fa4111d5e5c66c8b05e6baf4
Ramunas Jurgilas <ramunas.jurgilas@gmail.com>
2018-06-26 16:09:19 +0300

    Initial project was added.

---
+++ musicbrainz-places/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  musicbrainz-places
+//
+//  Created by Ramunas Jurgilas on 2018-06-26.
+//  Copyright © 2018 Ramūnas Jurgilas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
